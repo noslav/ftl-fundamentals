34415b3f805bf4588608f9db2e9cdb9dab968ed7
noslav <pranay.valson@gmail.com>
2020-10-19 00:31:05 +0200

    solves part 1 with tests
    Signed-off-by: noslav <pranay.valson@gmail.com>

--- calculator.go
+++ calculator.go
@@ -1,6 +1,10 @@
 // Package calculator provides a library for simple calculations in Go.
 package calculator
 
+import (
+	"fmt"
+)
+
 // Add takes two numbers and returns the result of adding them together.
 func Add(a, b float64) float64 {
 	return a + b
@@ -16,4 +20,13 @@ func Subtract(a, b float64) float64 {
 // the first. 
 func Multiply(a, b float64) float64 {
 	return a * b
+}
+
+// Divide takes two values and returns the result of multuiplying the second with
+// the first. 
+func Divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("bad input: %f, %f (division by zero is undefined)", a, b)
+	}
+	return a / b, nil
 }
--- calculator_test.go
+++ calculator_test.go
@@ -1,6 +1,7 @@
 package calculator_test
 
 import (
+	"fmt"
 	"calculator"
 	"testing"
 )
@@ -8,20 +9,22 @@ import (
 type testCase struct {
 	a, b float64
 	want float64
+	errExpected bool
 	}
 
 
 func TestAdd(t *testing.T) {
 	t.Parallel()
 	testCases := []testCase {
-		{ a: 2, b: 2, want: 4 },
+		{ a: 2, b: 2, want: 5 },
 		{ a: 1, b: 1, want: 2 },
 		{ a: 5, b: 0, want: 5 },
+		{ a: 0, b: 0, want: 0 },
 	}
 	for _, tc:= range testCases {
 		got := calculator.Add(tc.a, tc.b)
 		if tc.want != got {
-			t.Errorf("Add(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+			t.Errorf("Add(%f, %f): want %f, got %f ", tc.a, tc.b, tc.want, got)
 		}
 
 	} 
@@ -31,19 +34,53 @@ func TestAdd(t *testing.T) {
 
 func TestSubtract(t *testing.T) {
 	t.Parallel()
-	var want float64 = 2
-	got := calculator.Subtract(4, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+	testCases := []testCase {
+		{ a: 2, b: 1, want: 1 },
+		{ a: 1, b: 1, want: 0 },
+		{ a: 0, b: 1, want: -1 },
 	}
+	for _, tc:= range testCases {
+		got := calculator.Subtract(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Subtract(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
+
+	} 
 }
 
 func TestMultiply(t *testing.T) {
 	t.Parallel()
-	var want float64 = 6
-	got := calculator.Multiply(2, 3)
-	if want != got {
-		t.Errorf("want %f , got %f", want, got)
+	testCases := []testCase {
+		{ a: 2, b: 2, want: 4 },
+		{ a: 1, b: 1, want: 1 },
+		{ a: 5, b: 0, want: 0 },
+		{ a: 2.5, b: 2.5, want: 6.25},
 	}
+	for _, tc:= range testCases {
+		got := calculator.Multiply(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Multiply(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
+
+	} 
 }
 
+func TestDivide(t *testing.T) {
+	t.Parallel()
+	testCases := []testCase {
+		{ a: 2, b: 2, want: 1, errExpected: false },
+		{ a: 1, b: 1, want: 1, errExpected: false },
+		{ a: 2.5, b: 2.5, want:1, errExpected: false},
+		{ a: 5, b: 0, want:0, errExpected: true},
+	}
+	for _, tc:= range testCases {
+		got, err := calculator.Divide(tc.a, tc.b)
+		fmt.Printf(" error: %s,", err)
+		if !tc.errExpected && tc.want != got {
+			t.Errorf("Divide(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
+		if tc.errExpected !=  (err != nil) {
+			t.Fatalf("Divide(%f, %f): unexpected error status: %v", tc.a, tc.b, err)
+		}
+	} 
+}
